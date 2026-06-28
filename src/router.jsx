de7a8527b3c8c3843e2184a7819a1d14f18157cb
Ahmad-AlMasri1 <ahmad.mcksj@gmail.com>
2026-06-29 00:24:23 +0300

    Building the foundation of the project

---
+++ src/router.jsx
@@ -0,0 +1,40 @@
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import MainLayout from "./layout/MainLayout";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Cart from "./pages/cart/Cart";
+import Products from "./pages/products/Products";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+        {
+            index:true,
+            element:<Home />
+        },
+        {
+            path:"products",
+            element:<Products />
+        },
+        {
+            path:"cart",
+            element:<Cart />
+        }
+        ,
+        {
+            path:"login",
+            element:<Login />
+        }
+        ,
+        {
+            path:"register",
+            element:<Register />
+        }
+    ]
+  },
+]);
+
+export default router;
