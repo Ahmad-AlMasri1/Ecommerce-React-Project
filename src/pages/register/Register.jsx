20b80bebfe0debcdbfeab3fa34d79015a15dc922
Ahmad-AlMasri1 <ahmad.mcksj@gmail.com>
2026-06-29 05:46:38 +0300

    Completion of the basic construction process at the registration page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
-
+import { Box , TextField, Typography , Button} from '@mui/material'
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
 export default function Register() {
+
+          const {register , handleSubmit} = useForm();
+          const RegisterForm = async(data)=>{
+            try{
+              const response = await axios.post(`${import.meta.env.VITE_BURL}/auth/Account/Register`,data)
+            }catch(err){
+              console.log(err);
+            }
+          }
+
   return (
-    <div>Register</div>
+    <Box component="section" >
+      <Typography component="h1" variant="h2">
+          Register
+      </Typography>
+
+      <Box onSubmit={handleSubmit(RegisterForm)} component="form" sx={{marginTop:2 , display:'flex' , gap:4 , flexDirection:'column'}}>
+          <TextField fullWidth {...register("User Name")} label="User Name" variant='outlined' />
+          <TextField fullWidth {...register("Email")} label="Email" variant='outlined' />
+          <TextField fullWidth {...register("Password")} label="Password" variant='outlined' />
+          <TextField fullWidth {...register("Full Name")} label="Full Name" variant='outlined' />
+          <TextField fullWidth {...register("Phone Number")} label="Phone Number" variant='outlined' />
+          <Button variant="contained" type="submit">
+            Register
+          </Button>
+      </Box>
+    </Box>
   )
 }
